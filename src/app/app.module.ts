9d7e88f864f511d8c848d3c8fa0e549048dc9dc1
ctriou <ctriou@gmail.com>
2018-07-29 14:15:43 -0500

    * Stub out the UI look * Pulled in Bootstrap and ng-bootstrap, and established basic theme that somewhat matches cardplayer.com * Copied flags from cardplayer.com, and established helper classes for displaying them * Enabled support for IE9+

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent } from './app.component';
+import { PlayerLeaderboardComponent } from './components/player-leaderboard/player-leaderboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayerLeaderboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
